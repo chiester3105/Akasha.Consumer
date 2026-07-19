bd8ac1663f44991f60faf72d0acc6804d9ab20d6
chiester3105 <chiester3105@gmail.com>
2026-07-19 12:39:00 +0300

    runner
    adding services to DI
    applying migrations

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Akasha.Consumer.Services;
+using Akasha.Consumer.Workers;
+using DbUp;
+
 namespace Akasha.Consumer
 {
     public class Program
@@ -5,12 +9,42 @@ namespace Akasha.Consumer
         public static void Main(string[] args)
         {
             var builder = Host.CreateApplicationBuilder(args);
-            //builder.Services.AddHostedService<Worker>();
 
+            builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            builder.Services.AddSingleton<MatchResultRepository>();
+            builder.Services.AddHostedService<KafkaConsumerWorker>();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+            
             var host = builder.Build();
-            host.Run();
 
-           
+            using (var scope = host.Services.CreateScope())
+            {
+                var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+                var connectionString = config["Postgres"];
+
+                var upgrader = DeployChanges.To.PostgresqlDatabase(connectionString)
+                    .WithScriptsFromFileSystem("./Migrations")
+                    .LogToConsole()
+                    .Build();
+
+                var result = upgrader.PerformUpgrade();
+
+                if(!result.Successful)
+                {
+                    Console.WriteLine($"Migration failed {result.Error}");
+                    Environment.Exit(1);
+                }
+                else
+                {
+                    Console.WriteLine("Migration applied");
+                }
+            }
+
+            host.Run();
         }
     }
 }
